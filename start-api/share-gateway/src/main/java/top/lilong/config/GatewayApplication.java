b8c95f55389a1434337a9af6a10f64921e088c05
lilong <2199630705@qq.com>
2023-10-07 11:27:35 +0800

    网关

---
+++ start-api/share-gateway/src/main/java/top/lilong/config/GatewayApplication.java
@@ -0,0 +1,27 @@
+package top.lilong.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+/**
+ * @version 1.0
+ * @Author 李龙
+ * @Date 2023/10/7 11:04
+ * @注释
+ */
+@SpringBootApplication
+@ComponentScan("top.lilong")
+@Slf4j
+public class GatewayApplication {
+ public static void main(String[] args) {
+  SpringApplication app=new SpringApplication(GatewayApplication.class);
+  Environment env = app.run(args).getEnvironment();
+  log.info("网关启动成功");
+  log.info("网关地址：http://127.0.0.1:{}",env.getProperty("server.port"));
+
+ }
+}
