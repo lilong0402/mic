8e98a6a81363059753ba9c89683882f6ff5e7a91
lilong <2199630705@qq.com>
2023-10-21 16:11:25 +0800

    为gateway 增加登录校验过滤器

---
+++ start-api/share-gateway/src/main/java/top/lilong/config/LoginUserFilter.java
@@ -0,0 +1,55 @@
+package top.lilong.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import top.lilong.util.JwtUtil;
+
+/**
+ * @version 1.0
+ * @Author 李龙
+ * @Date 2023/10/8 17:00
+ * @注释
+ */
+@Component
+@Slf4j
+public class LoginUserFilter implements Ordered, GlobalFilter {
+ @Override
+ public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+  String path = exchange.getRequest().getURI().getPath();
+  if (path.contains("/admin")|| path.contains("/hello")||path.contains("/user-service/user/login")||path.contains("user-service/user/register")){
+   log.info("不需要登录验证：{}",path);
+   return chain
+           .filter(exchange);
+  }else {
+   log.info("需要登录验证: {}",path);
+  }
+  String token=exchange.getRequest().getHeaders().getFirst("token");
+  log.info("会员登录验证开始，token: {}",token);
+  if (token == null || token.isEmpty()){
+   log.info("token 为空，请求被拦截");
+   exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+   return exchange.getResponse().setComplete();
+  }
+
+  boolean validate = JwtUtil.validate(token);
+  if (validate){
+   log.info("token 有效，放行该请求");
+   return chain.filter(exchange);
+  }else {
+   log.warn("token 无效，请求被拦截");
+   exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+   return exchange.getResponse().setComplete();
+  }
+ }
+
+ @Override
+ public int getOrder() {
+  return 0;
+ }
+}
--- start-api/share-gateway/src/main/java/top/lilong/config/Test2Filter.java
+++ start-api/share-gateway/src/main/java/top/lilong/config/Test2Filter.java
@@ -1,32 +1,32 @@
-package top.lilong.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-/**
- * @version 1.0
- * @Author 李龙
- * @Date 2023/10/8 16:55
- * @注释
- */
-@Component
-@Slf4j
-public class Test2Filter implements GlobalFilter, Ordered {
-
-
- @Override
- public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-  log.info("Test2Filter");
-  return chain.filter(exchange);
- }
-
- @Override
- public int getOrder() {
-  return 1;
- }
-}
+//package top.lilong.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+///**
+// * @version 1.0
+// * @Author 李龙
+// * @Date 2023/10/8 16:55
+// * @注释
+// */
+//@Component
+//@Slf4j
+//public class Test2Filter implements GlobalFilter, Ordered {
+//
+//
+// @Override
+// public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//  log.info("Test2Filter");
+//  return chain.filter(exchange);
+// }
+//
+// @Override
+// public int getOrder() {
+//  return 1;
+// }
+//}
